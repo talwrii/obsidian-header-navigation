6efccdebaa2ed2271219b3baa0eddccdc7b44317
Tal Wrii <talwrii@gmail.com>
2024-11-28 21:02:57 +0100

    header navigation plugin
    Implementation

--- README.md
+++ README.md
@@ -1,94 +1,31 @@
-# Obsidian Sample Plugin
+# Obsidian header navigation plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+This plugin lets you quickly navigate between headers and fold them.
+It provides a command to toggle folding of a header.
 
-This project uses TypeScript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in TypeScript Definition format, which contains TSDoc comments describing what it does.
+# Installation
+This plugin should be available through obsidian community plugins.
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+Alternatively you can install it directly by
 
-## First time developing plugins?
+1. Cloning the git repository into `.obsidian/plugins` in your vault
+1. Running `npm install`, `npm run dev` in the repository
+1. Reloading obsidian
+1. Enabling the plugin in the community plugins tab of settings.
 
-Quick starting guide for new plugin devs:
+# Using
+Press C-P to open the command palette and search for "header nagivation".
+This will show the commands that this plugin provides.
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+I would suggest binding these commands to hotkeys. You might find a plugin like [sequence hotkeys](https://github.com/moolmanruan/obsidian-sequence-hotkeys) useful, this allows you to define a "prefix" key such as C-m for motion commands so you can then assing C-m Up etc for moving around headers.
 
-## Releasing new releases
+# Influences
+This is very much influenced by the functions provided by emacs <a href="https://orgmode.org/">org-mode</a>
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+# About me
+I amongst other things work on productivity tools and AI tools related to reading.
+If that sounds interesting you can follow me on <a href="https://x.com/readwithai">twitter</a> or <a href="https://bsky.app/profile/readwithai.bsky.social">bluesky</a>.
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+I write about these topics on <a href="https://readwithai.substack.com/readwithai">substack</a>.
 
-## Adding your plugin to the community plugin list
-
-- Check the [plugin guidelines](https://docs.obsidian.md/Plugins/Releasing/Plugin+guidelines).
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-## How to use
-
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
-
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+If you find *this* piece of software useful. Maybe give me money (like $2 dollars?) on my <a href="ko-fi.com/readwithai">kofi</a>.
--- main.ts
+++ main.ts
@@ -1,134 +1,220 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Plugin, EditorPosition } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+import { remark } from 'remark';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface RemarkPosition {
+    line: number,
+    column: number,
+    offset: number,
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const parseRemarkPos = (p: RemarkPosition) => {
+    return { line: p.line - 1, ch: p.column }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class HeaderNavPlugin extends Plugin {
+    async onload() {
+        this.addCommand({
+            id: 'header-navigation-back',
+            name: 'Jump back to heading',
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                this.backToHeading(editor)
+            }
+        });
 
-	async onload() {
-		await this.loadSettings();
+        this.addCommand({
+            id: 'header-navigation-forward',
+            name: 'Jump to next heading at the same level',
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                this.forwardHeading(editor)
+            }
+        });
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        this.addCommand({
+            id: 'header-navigation-toggle-fold',
+            name: 'Toggle header folding',
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                this.toggleFold(editor)
+            }
+        });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        this.addCommand({
+            id: 'header-navigation-backward',
+            name: 'Jump to previous heading at the same level',
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                this.backwardHeading(editor)
+            }
+        });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        this.addCommand({
+            id: 'header-navigation-parent',
+            name: 'Jump to parent heading',
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                this.upHeading(editor)
+            }
+        })
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        this.addCommand({
+            id: 'header-navigation-child',
+            name: 'Jump to the first child of current header',
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                this.firstChild(editor)
+            }
+        })
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    backToHeading(editor: Editor) {
+        // Go back to a heading
+        const tree = this.getTree(editor)
+        const point = this.findHeadingBefore(editor, tree, editor.getCursor())
+        if (point) {
+            editor.setCursor(point)
+        }
+    }
 
-	onunload() {
+    forwardHeading(editor: Editor) {
+        // Go forward to the next heading of the same depth
+        const depth = this.depth(editor, editor.getCursor().line)
+        const tree = this.getTree(editor)
+        const point = this.findHeadingAfter(editor, tree, editor.getCursor(), depth)
+        if (point) {
+            editor.setCursor(point)
+        }
+    }
 
-	}
+    backwardHeading(editor: Editor) {
+        //"Go backward to heading of the same depth
+        const depth = this.depth(editor, editor.getCursor().line)
+        const tree = this.getTree(editor)
+        const point = this.findHeadingBefore(editor, tree, editor.getCursor(), depth)
+        if (point) {
+            editor.setCursor(point)
+        }
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    upHeading(editor: Editor) {
+        // Go up heading
+        const depth = this.depth(editor, editor.getCursor().line)
+        const tree = this.getTree(editor)
+        const point = this.findHeadingBefore(editor, tree, editor.getCursor(), depth - 1)
+        if (point) {
+            editor.setCursor(point)
+        }
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+    findHeadingBefore(editor: Editor, tree: Object, position: EditorPosition, depth?: number): EditorPosition | undefined {
+        // In a remark tree, find the header before this posiition
+        var before: EditorPosition | undefined = undefined;
+        const headings = this.flatHeadings(tree)
+        headings.forEach((p) => {
+            if (p.line < position.line) {
+                const p_depth = this.depth(editor, p.line)
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+                if (depth === undefined || p_depth === depth) {
+                    before = p
+                }
+            }
+        });
+        return before
+    }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    findHeadingAfter(editor: Editor, tree: Object, position: EditorPosition, depth?: number) {
+        // In a remark tree, find the header after this posiition
+        // with an optional depth
+        let after: any = undefined
+        const headings = this.flatHeadings(tree)
+        headings.forEach((p) => {
+            if (p.line > position.line) {
+                const p_depth = this.depth(editor, p.line)
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+                if (depth === undefined || p_depth === depth) {
+                    after = after || p
+                }
+            }
+        });
+        return after
+    }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    flatHeadings(tree: any): Array<EditorPosition> {
+        let result = []
+        if (tree.type == "heading") {
+            result.push(parseRemarkPos(tree.position.start))
+        }
+        for (var i = 0; i < (tree.children || []).length; i++) {
+            let child = tree.children[i]
+            result.push(...this.flatHeadings(child))
+        }
+        return result
+    }
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
 
-	display(): void {
-		const {containerEl} = this;
+    firstChild(editor: Editor) {
+        //"Go backward to heading of the same depth
+        const depth = this.depth(editor, editor.getCursor().line)
+        const tree = this.getTree(editor)
+        const point = this.findHeadingAfter(editor, tree, editor.getCursor())
 
-		containerEl.empty();
+        // @ts-ignore
+        if (depth && point && (this.depth(editor, point.line) > depth)) {
+            editor.setCursor(point)
+        }
+    }
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    pointMax(editor: Editor): EditorPosition {
+        const lastLine = editor.lastLine()
+        const lastChar = editor.getLine(lastLine).length
+        return { line: lastLine, ch: lastChar }
+    }
+
+
+    toggleFold(editor: Editor): void {
+        const line = editor.getCursor().line
+        if (this.lineFolded(editor, line)) {
+            //@ts-ignore
+            this.app.commands.executeCommandById("editor:fold-less")
+        } else {
+            //@ts-ignore
+            this.app.commands.executeCommandById("editor:fold-more")
+        }
+    }
+
+    lineFolded(editor: Editor, line: number): boolean {
+        // This seems to be a "secret" api
+        // @ts-ignore
+        const offsets = editor.getFoldOffsets()
+
+        for (const offset of offsets) {
+            const pos = editor.offsetToPos(offset)
+            if (pos.line == line) {
+                return true
+            }
+        }
+        return false
+    }
+
+
+    depth(editor: Editor, line: number): number | undefined {
+        // Find the header depth of a line
+        const content = editor.getLine(line)
+
+        for (var i = 0; i < content.length; i++) {
+            if (content[i] != "#") {
+                break
+            }
+        }
+
+        if (i === 0) {
+            return undefined
+        } else {
+            return i;
+        }
+    }
+
+    getTree(editor: Editor): Object {
+        const body = editor.getRange({ line: 0, ch: 0 }, this.pointMax(editor))
+        return remark.parse(body)
+    }
+
 }
